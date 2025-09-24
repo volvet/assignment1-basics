5f9f6807a1e3b95980c674655a7e7c6f3797b644
volvet <volvet@163.com>
2025-09-24 14:58:54 +0800

    test_swiglu passed

--- cs336_basics/__init__.py
+++ cs336_basics/__init__.py
@@ -4,5 +4,6 @@ from .data_loader import DataLoader
 from .linear import Linear
 from .embedding import Embedding
 from .rms_norm import RMSNorm
+from .position_wise_feed_forward import PositionWiseFeedForward
 
 __version__ = importlib.metadata.version("cs336_basics")
--- tests/adapters.py
+++ tests/adapters.py
@@ -13,6 +13,7 @@ from cs336_basics import DataLoader
 from cs336_basics import Linear
 from cs336_basics import Embedding
 from cs336_basics import RMSNorm
+from cs336_basics import PositionWiseFeedForward
 
 def run_linear(
     d_in: int,
@@ -89,7 +90,11 @@ def run_swiglu(
     # swiglu.w1.weight.data = w1_weight
     # swiglu.w2.weight.data = w2_weight
     # swiglu.w3.weight.data = w3_weight
-    raise NotImplementedError
+    swiglu = PositionWiseFeedForward(d_model, d_ff)
+    swiglu.linear1.weight.data.copy_(w1_weight)
+    swiglu.linear2.weight.data.copy_(w2_weight)
+    swiglu.linear3.weight.data.copy_(w3_weight)
+    return swiglu(in_features)
 
 
 def run_scaled_dot_product_attention(
