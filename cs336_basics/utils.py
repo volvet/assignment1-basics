1ed4a83ce6a233e03999d6f5586f219fffeb8a35
volvet <volvet@163.com>
2025-10-01 07:50:23 +0800

    test_lr_cosine_schedule passed

--- cs336_basics/utils.py
+++ cs336_basics/utils.py
@@ -23,3 +23,21 @@ def clip_gradients(parameters : torch.nn.Parameter, max_l2_norm : float) -> None
     if clip_coef < 1:
         for p in parameters_with_grad:
             p.grad.data.mul_(clip_coef)
+
+
+def lr_cosine_schedule(current_iter: int,
+                       max_learning_rate: float,
+                       min_learning_rate: float,
+                       warmup_iters: int,
+                       cosine_cycle_iters: int) -> float:
+    if current_iter < warmup_iters:
+        lr = max_learning_rate * current_iter / warmup_iters
+        # lr = max(lr, min_learning_rate)
+        return lr
+
+    if current_iter > cosine_cycle_iters:
+        return min_learning_rate
+
+    cosine_decay = (1 + np.cos(np.pi *(current_iter - warmup_iters) / (cosine_cycle_iters - warmup_iters))) / 2.0
+    lr = min_learning_rate + (max_learning_rate - min_learning_rate) * cosine_decay
+    return lr
